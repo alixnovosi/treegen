01738ae4c58851ed79f9580db46882816f33f8db
Andrew Michaud <dev@mail.andrewmichaud.com>
2017-11-06 14:35:52 -0800

    supposed to be a library, oops

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-# tree_bot
-bot drawing pretty trees
-
-Created by Andrew Michaud for #NaBoMaMo 2017
+# tree_gen
+library for drawing pretty trees
--- setup.py
+++ setup.py
@@ -9,16 +9,16 @@ with open(path.join(HERE, "VERSION"), encoding="utf-8") as f:
     VERSION = f.read().strip()
 
 setup(author="Andrew Michaud",
-      author_email="bots+tree@mail.andrewmichaud.com",
-      url="https://github.com/andrewmichaud/tree_bot",
+      author_email="dev@mail.andrewmichaud.com",
+      url="https://github.com/andrewmichaud/tree_gen",
 
-      entry_points={"console_scripts": ["tree__bot = tree_bot.__main__:main"]},
+      entry_points={"console_scripts": ["tree__gen = tree_gen.__main__:main"]},
 
       install_requires=["botskeleton>=1.1.0", "Pillow"],
 
       license="BSD3",
 
-      name="tree_bot",
+      name="tree_gen",
 
       packages=find_packages(),
 
